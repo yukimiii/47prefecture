0300e0bc0d6059a63589734e6f7c59f3f1ab211b
snara-42 <77873284+snara-42@users.noreply.github.com>
2021-04-20 16:47:33 +0900

    update ibaraki 2020/04/03

--- ibaraki.c
+++ ibaraki.c
@@ -1,8 +1,17 @@
-int main(){
+#include <stdio.h>
+
+char  *f(char *s, int *a, int n)
+{
+  for(int i=0; i<n; i++)
+    s[i] += a[i];
+  return (s);
+}
+
+int  main()
+{
   char s[] = "Hello, world!\n";
 
-  write(1, s, 14);
-  s[0] += 6; s[1] -= 4; s[2] = s[3] += 8;
-  write(1, s, 14);
+  printf("%s", s);
+  printf("%s", f(s, (int[]){6,-4,8,8}, 4));
   return (0);
 }
