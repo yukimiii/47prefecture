d55a727d8933404dce314323008b07fa426f2c18
snara-42 <77873284+snara-42@users.noreply.github.com>
2021-09-01 18:23:14 +0900

    kyoto 2021/08/29 fix

--- kyoto.py
+++ kyoto.py
@@ -3,7 +3,6 @@ import sys, requests, json
 address = input() if len(sys.argv)==1 else " ".join(sys.argv[1:])
 url = "http://api.geodosu.com/v2/geodosu2"
 params = {"output":"json","address":address}
-main
 res = requests.get(url,params=params)
 data = json.loads(res.text)
 print(json.dumps(data,ensure_ascii=False,indent=2))
